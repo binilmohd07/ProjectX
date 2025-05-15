ea2215626796281dc72b78caa1386a8ce74dcd83
binilmohamed07 <binilmohamed07@gmail.com>
2025-05-16 00:41:31 +0530

    feat : Implement auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
---
+++ src/app/services/auth.guard.ts
@@ -0,0 +1,28 @@
+import {
+  CanActivate,
+  Router,
+  UrlTree,
+} from '@angular/router';
+import { auth } from '../firebase.config';
+import { onAuthStateChanged } from 'firebase/auth';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) { }
+
+  canActivate(): Promise<boolean | UrlTree> {
+    return new Promise((resolve) => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        unsubscribe();
+        if (user) {
+          resolve(true);
+        } else {
+          resolve(this.router.createUrlTree(['/']));
+        }
+      });
+    });
+  }
+}
