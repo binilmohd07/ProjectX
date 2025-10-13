b686be4a4c63e509023dcbefcc8932c9e3548758
binilmohamed07 <binilmohamed07@gmail.com>
2025-10-13 06:05:57 +0530

    Updates in styling in to dos

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,9 @@
+
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { firebaseConfig } from '../../firebase.config';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 @Component({
   selector: 'app-login',
@@ -17,10 +19,25 @@ export class LoginComponent {
   user: any;
 
   signInWithGoogle() {
+    // Only redirect if both Firebase user and calendar token exist
+    if (this.authService.getUser()) {
+      const existingToken = localStorage.getItem('google_calendar_token');
+      if (existingToken) {
+        this.router.navigate(['/dashboard']);
+        return;
+      }
+    }
+    // Otherwise, always run the sign-in flow
     this.authService.loginWithGoogle().then(async res => {
       this.user = res.user;
-      // After Firebase login, also request Google Calendar access
-      await this.initGoogleCalendarAuth();
+      const credential = GoogleAuthProvider.credentialFromResult(res);
+      const accessToken = credential?.accessToken;
+      if (accessToken) {
+        localStorage.setItem('google_calendar_token', accessToken);
+        this.router.navigate(['/dashboard']);
+      } else {
+        await this.initGoogleCalendarAuth();
+      }
     });
   }
 
